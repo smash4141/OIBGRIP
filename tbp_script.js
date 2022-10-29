d16e2dddbdb515cd939b21be771ea5f4f155250b
Akash Gupta <smashgupta2002@gmail.com>
2022-10-29 13:48:00 +0530

    Add files via upload

---
+++ tbp_script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings: ["An Inspiration", "Missile Man ", "Animal Welfare Activist", "Scientist","Member of Parliament", "President of India", "Writer",],
+    typeSpeed:100,
+    backSpeed:80,
+    loop:true
+});
